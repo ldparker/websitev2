90ee30e6e39face9bcdc3702637367f9f7210992
Logan Parker <loganparker@Logans-MacBook-Pro.local>
2017-01-27 16:57:03 -0800

    third commit

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     
     $('#h1header').delay(900).fadeIn(800);
     $('.socialsa').delay(2000).fadeIn(800);
+    $('.arrow').delay(2000).fadeIn(800);
     
     $('.socialsa').hover(function() {
         $(this).addClass('hovered');
@@ -22,16 +23,36 @@ $(document).ready(function() {
         $(this).removeClass('hovered');
     });
     
+    $('.arrowpos').hover(function() {
+        $(this).addClass('hovered');
+    },
+    function() {
+        $(this).removeClass('hovered');
+    });
+    
+    $('.arrowpos').click(function() {
+        $('html , body').animate( {
+            scrollTop: $('.aboutoutter').offset().top
+        }, 1500);
+    });
+    
     $('#button1').click(function() {
-        $('.library').slideToggle(500);
+        $('.library').slideToggle(100);
+        $('.youtube').hide(100);
+        $('.hometown').hide(100);
+        
     });
     
     $('#button2').click(function() {
-        $('.youtube').slideToggle(500);
+        $('.youtube').slideToggle(100);
+        $('.library').hide(100);
+        $('.hometown').hide(100);
     });
     
     $('#button3').click(function() {
-        $('.hometown').slideToggle(500);
+        $('.hometown').slideToggle(100);
+        $('.library').hide(100);
+        $('.youtube').hide(100);
     });
     
     
