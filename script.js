9c9affebb6752aac11735ad8e8a636167d8aa93f
Logan Parker <loganparker@Logans-MacBook-Pro.local>
2017-01-29 20:59:18 -0800

    Fourth commit

--- script.js
+++ script.js
@@ -36,23 +36,36 @@ $(document).ready(function() {
         }, 1500);
     });
     
+    $('#button1, #button2, #button3').click(function() {
+        $('html , body').animate( {
+            scrollTop: $('.tourblurb').offset().top
+        }, 500);
+    });
+    
     $('#button1').click(function() {
-        $('.library').slideToggle(100);
-        $('.youtube').hide(100);
-        $('.hometown').hide(100);
+        $('.library').slideToggle(200);
+        $('.youtube').hide(200);
+        $('.hometown').hide(200);
         
     });
     
     $('#button2').click(function() {
-        $('.youtube').slideToggle(100);
-        $('.library').hide(100);
-        $('.hometown').hide(100);
+        $('.youtube').slideToggle(200);
+        $('.library').hide(200);
+        $('.hometown').hide(200);
     });
     
     $('#button3').click(function() {
-        $('.hometown').slideToggle(100);
-        $('.library').hide(100);
-        $('.youtube').hide(100);
+        $('.hometown').slideToggle(200);
+        $('.library').hide(200);
+        $('.youtube').hide(200);
+    });
+    
+    $('.socialsaend').hover(function() {
+        $(this).addClass('hoveredend');
+    },
+    function() {
+        $(this).removeClass('hoveredend');
     });
     
     
